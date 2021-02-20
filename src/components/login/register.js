3d2927cd1fdb5b9dd816ad2d5960181cb1d93e05
Saurav <saurav@localhost.localdomain>
2021-02-20 22:00:01 +0530

    fixing reg

--- src/components/login/register.js
+++ src/components/login/register.js
@@ -5,21 +5,41 @@ import {useStateValue} from "../../stateProvider"
 
 import axios from 'axios'
 
+import { useHistory } from "react-router-dom";
+
 function Register() {
     const [, dispatch] = useStateValue()
+    const [error, setError] = useState()
+    let history = useHistory();
+
 
     const login = (e)=>{
         e.preventDefault()
-        axios.post(
-            "https://suman-ecommerce-api.herokuapp.com/api-token-auth/",
-            new FormData(document.getElementById("register_form"))
-        ).then((res)=>{
-            console.log(res)
-            dispatch({
-                type: 'SET_USER',
-                token: res.data.token
+        if(document.getElementById("password1").value === document.getElementById("password2").value){
+            axios.post(
+                "https://suman-ecommerce-api.herokuapp.com/api-token-auth/",
+                new FormData(document.getElementById("register_form"))
+            ).then((res)=>{
+                if (res.status === 400){
+                    axios.post(
+                        "https://suman-ecommerce-api.herokuapp.com/register/",
+                        new FormData(document.getElementById("register_form"))
+                    ).then((res)=>{
+                        history.push("/login");
+                    })
+                }
+                else{
+                    setError("Username Already Taken")
+                }
+                dispatch({
+                    type: 'SET_USER',
+                    token: res.data.token
+                })
             })
-        })  
+        }
+        else{
+            setError("Passwords didn't matched")
+        }
     }
 
 
@@ -33,9 +53,9 @@ function Register() {
                     <label htmlFor="username">Username</label>
                     <input type="text" name="username"/>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" id="password1"/>
                     <label htmlFor="password">Reenter the password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" id="password2"/>
                     <button type="submit" className="login_submit">SUBMIT</button>
                 </form>
                 <p className="login_register">Already have an account ? login <a href="/login">here</a></p>
