06452c8007ec771162c18026ae541953c82497f9
Saurav <saurav@localhost.localdomain>
2021-03-02 16:21:47 +0530

    fixing reg 8

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -49,7 +49,7 @@ function Cart() {
         }
         
         
-    },[dispatch, cookies.woodToken, cookies.woodToken?.token],user, user?.username);
+    },[dispatch, cookies.woodToken, cookies.woodToken?.token, user?.username]);
 
     const buy = ()=>{
         if (cookies.woodToken){
